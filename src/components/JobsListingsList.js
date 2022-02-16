8a054489be3782b6532806841269ff5df39920e8
simoneclr <simone.clclr@gmail.com>
2022-02-16 16:44:17 +0100

    Add basic filtering logic

--- src/components/JobsListingsList.js
+++ src/components/JobsListingsList.js
@@ -9,6 +9,9 @@ function JobListingsList() {
 	// State variable containing all jobs listings
 	const [jobs, setJobs] = useState([])
 
+	// State variable containing the list of active filters
+	const [filters, setFilters] = useState([])
+
 	// When the component mounts, get the jobs data  
 	useEffect(() => {
 		getData().then(data => {
@@ -16,7 +19,19 @@ function JobListingsList() {
 		})
 	}, [])
 	
-	return jobs.map(j => <JobListing key={j.id} job={j}/>)
+	return jobs.filter(j => {
+		let pass = true
+		
+		// Join all filterable tags in a single array for convenience 
+		let tags = [j.role, j.level, ...j.languages, ...j.tools]
+
+		// Iterate on all filters, and check if they are all present in the current job's tags
+		filters.forEach(f => {
+			pass = pass && tags.indexOf(f) >= 0
+		})
+
+		return pass
+	}).map(j => <JobListing key={j.id} job={j}/>)
 }
 
 export default JobListingsList
