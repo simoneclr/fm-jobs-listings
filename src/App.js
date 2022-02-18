69406e00944a76b515a75d8445f901d118f06e38
simoneclr <simone.clclr@gmail.com>
2022-02-18 16:13:56 +0100

    Update layout and styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React from "react"
 import JobListings from "./components/JobListings"
 
 function App() {
-	return <JobListings/>
+	return (
+		<React.Fragment>
+			<div className="bg-header">
+				<img src="./images/bg-header-desktop.svg" alt=""/>
+			</div>
+
+			<div className="container">
+				<JobListings/>
+			</div>	
+		</React.Fragment>
+	)
 }
 
 export default App
